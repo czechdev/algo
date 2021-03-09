75da3def79d1ebf430ad5ed6220defca72c8134a
czechdev <67641956+czechdev@users.noreply.github.com>
2021-03-09 01:31:25 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,29 @@
+const answer = arr => {
+    let lengArr = []
+    let resultArr = []
+    arr.sort((a, b) => a - b)
+    console.log(arr)
+    let leng = 1
+    for (let i = 0; i < arr.length; i++) {
+        if(arr[i] === arr[i + 1]) {
+            leng = leng + 1
+        }
+        else {
+            lengArr.push(leng)
+            leng = 1
+        }
+    }
+    console.log(lengArr)
+    for (let j = 0; j < lengArr.length; j++) {
+        if (lengArr[j] > 1) {
+            resultArr.push(arr.splice(0, lengArr[j]))
+        } 
+        else if (lengArr[j] === 1) {
+            resultArr.push(arr[0])
+            arr.shift()
+        }  
+    }
+    
+    console.log(resultArr)
+    return resultArr
+}
